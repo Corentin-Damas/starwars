68fbc4ab278c13dc18a3343cbaac4d3a4e60878e
Nazemrap <corentin.damas@gmail.com>
2023-05-12 17:30:15 +0200

    add slides elements & script next/pre

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 "use strict";
 
-const leftArrow = document.querySelector(".left-arrow");
-const rightArrow = document.querySelector(".right-arrow");
+const prevArrow = document.querySelector(".previous-arrow");
+const nextArrow = document.querySelector(".next-arrow");
+const nbsteps = document.querySelectorAll(".line");
+
+console.log("helloword!");
+let curentStepper = 1;
+
+nextArrow.addEventListener("click", function () {
+  console.log("next");
+  curentStepper++;
+  if (curentStepper > nbsteps.length) {
+    curentStepper = nbsteps.length;
+  }
+  update();
+});
+
+prevArrow.addEventListener("click", function () {
+  console.log("previ");
+  curentStepper--;
+  if (curentStepper < 1) {
+    curentStepper = 1;
+  }
+  update();
+});
+
+function update() {
+  nbsteps.forEach((step, index) => {
+    if (index < curentStepper) {
+      step.classList.add("active");
+    } else {
+      step.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+  if (curentStepper == nbsteps.length) {
+    nextArrow.disabled = true;
+  } else if (curentStepper <= 1) {
+    prevArrow.disabled = true;
+  } else {
+    nextArrow.disabled = false;
+    prevArrow.disabled = false;
+  }
+}
